d8e6dbdd60ddeb5a62fffc3a9aa73021bd636c2e
Zaptan <zaptan@gmail.com>
2016-09-23 15:02:01 -0700

    feature placeholders

--- src/pool.cpp
+++ src/pool.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
-const int sensorPin= 0;
-const int ledPin= 12;
-int liquid_level;
-int s, ticks;
+const int sensorPin = 0;
+const int ledPin = 12;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+const long interval = 1000;
+const int limit = 300;
+
+void turnOnWater();
+void turnOffWater();
+
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(sensorPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  liquid_level=0;
-  s=0;
+    Serial.begin(9600);
+    pinMode(sensorPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW);
 }
+
 /*
 the sensorPin returns a number based on how conductive whatever medium is
 touching the sensor. tapwater ranged from 200 to 320 pool water seems to go from
@@ -21,27 +26,47 @@ mapped the numbers to that. the sensor is slow it can take up to a min to settle
 on a value, it will always jump really high and then slowly come back down to
 settle on the actual value
 
-*/
+ */
 void loop() {
-  ticks++;
-  s = analogRead(sensorPin);
-  liquid_level = map(s, 258, 415, 0, 40);
-  if (liquid_level < 0) { liquid_level = 0; }
 
-  delay(1000);
+    currentMillis = millis();
 
-  if (liquid_level > 30) {
-    digitalWrite(ledPin, HIGH);
-    Serial.print("Hi : ");
-    Serial.print(liquid_level);
-    Serial.print(" | ");
-    Serial.println(s);
-  }
-  else {
-    digitalWrite(ledPin, LOW);
-    Serial.print("Low: ");
-    Serial.print(liquid_level);
-    Serial.print(" | ");
-    Serial.println(s);
-  }
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        Serial.println(waterRaw());
+        if (waterRaw() < limit) {
+            turnOnWater();
+        } else {
+            turnOffWater();
+        }
+    }
 }
+
+void tapStart() {
+    // tun on water
+    digitalWrite(ledPin, HIGH);
+}
+
+void tapStop() {
+    // tun off water
+    digitalWrite(ledPin, LOW);
+}
+
+
+    int waterLevel() {
+        return 30; //TODO: implement measurement
+    }
+
+    int waterRaw() {
+        return analogRead(sensorPin);
+    }
+
+    int waterTemp() {
+        return 0;
+    }
+    int airTemp() {
+        return 0;
+    }
+    int airHumidity() {
+        return 0;
+    }
