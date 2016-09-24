b25f3552f720eb7a4288b254dbbe613b7b8f4146
Zaptan <zaptan@gmail.com>
2016-09-24 16:59:54 -0700

    added DHT sensor

--- src/pool.cpp
+++ src/pool.cpp
@@ -3,7 +3,16 @@
 #include <SPI.h>
 #include <WiFi101.h>
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
+#define DHTPIN            5         // Pin which is connected to the DHT sensor.
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
 
 const int sensorPin = 0;
 const int ledPin = 12;
@@ -20,7 +29,8 @@ int waterTemp();
 int airTemp();
 int airHumidity();
 void printWifiData();
-
+void setupDHT();
+void readDHT();
 
 char ssid[] = WIFI_SSID;                     // your network SSID (name)
 char pass[] = WIFI_PASS;       // your network key
@@ -30,7 +40,7 @@ int status = WL_IDLE_STATUS;                     // the Wifi radio's status
 void setup() {
     WiFi.setPins(8,7,4,2); //correct pins for feather
     Serial.begin(9600);     // serial output for debug
-    pinMode(sensorPin, INPUT_PULLDOWN);
+    pinMode(sensorPin, INPUT);
     pinMode(ledPin, OUTPUT);
     while ( status != WL_CONNECTED) {
       Serial.print("Attempting to connect to WPA network, SSID: ");
@@ -40,6 +50,7 @@ void setup() {
       // wait 10 seconds for connection:
       delay(10000);
     }
+    setupDHT();
 }
 
 /*
@@ -62,6 +73,7 @@ void loop() {
             tapStop();
         }
     printWifiData();
+    readDHT();
     }
 }
 
@@ -95,7 +107,7 @@ void tapStop() {
     }
 
 void printWifiData() {
-  Serial.println("\n ****************************** "
+  Serial.println("\n ****************************** ");
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
@@ -126,4 +138,57 @@ void printWifiData() {
   Serial.print("Encryption Type:");
   Serial.println(encryption, HEX);
   Serial.println();
+}
+
+void setupDHT() {
+  dht.begin();
+  Serial.println("DHTxx Unified Sensor Example");
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.println("------------------------------------");
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.println("------------------------------------");
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void readDHT() {
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println("%");
+  }
 }
