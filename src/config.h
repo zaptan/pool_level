2dbb36e44983aa63cf733a056558905167460463
Zaptan <zaptan@gmail.com>
2016-09-24 16:25:44 -0700

    adding wifi

---
+++ src/config.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   config.h
+ * Author: zaptan
+ *
+ * Created on September 24, 2016, 1:00 PM
+ */
+
+#define IO_USERNAME    "your_username"
+#define IO_KEY         "your_key"
+
+#define WIFI_SSID       "your_ssid"
+#define WIFI_PASS       "your_pass"
+
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
--- src/pool.cpp
+++ src/pool.cpp
@@ -1,21 +1,45 @@
 #include <Arduino.h>
+#include "config.h"
+#include <SPI.h>
+#include <WiFi101.h>
+
+
 
 const int sensorPin = 0;
 const int ledPin = 12;
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
-const long interval = 1000;
+const long interval = 10000;
 const int limit = 300;
 
-void turnOnWater();
-void turnOffWater();
+void tapStart();
+void tapStop();
+int waterLevel();
+int waterRaw();
+int waterTemp();
+int airTemp();
+int airHumidity();
+void printWifiData();
+
+
+char ssid[] = WIFI_SSID;                     // your network SSID (name)
+char pass[] = WIFI_PASS;       // your network key
+int status = WL_IDLE_STATUS;                     // the Wifi radio's status
 
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(sensorPin, INPUT);
+    WiFi.setPins(8,7,4,2); //correct pins for feather
+    Serial.begin(9600);     // serial output for debug
+    pinMode(sensorPin, INPUT_PULLDOWN);
     pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LOW);
+    while ( status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to WPA network, SSID: ");
+      Serial.println(ssid);
+      status = WiFi.begin(ssid, pass);
+  
+      // wait 10 seconds for connection:
+      delay(10000);
+    }
 }
 
 /*
@@ -28,17 +52,16 @@ settle on the actual value
 
  */
 void loop() {
-
     currentMillis = millis();
-
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;
         Serial.println(waterRaw());
         if (waterRaw() < limit) {
-            turnOnWater();
+            tapStart();
         } else {
-            turnOffWater();
+            tapStop();
         }
+    printWifiData();
     }
 }
 
@@ -70,3 +93,37 @@ void tapStop() {
     int airHumidity() {
         return 0;
     }
+
+void printWifiData() {
+  Serial.println("\n ****************************** "
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
